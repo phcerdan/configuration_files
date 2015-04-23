01f6db288c44d23b1a7798d622f64c93a0aa1333
phcerdan <pablo.hernandez.cerdan@outlook.com>
2015-04-23 12:10:22 +1200

    Tmuxifier layouts init.

---
+++ tmuxifier/layouts/eclimd.window.sh
@@ -0,0 +1,23 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+window_root "~/repository_local"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "eclimd"
+
+# Split window into panes.
+# split_v 20
+split_h 20
+
+# Run commands.
+run_cmd "cd ~/Software/eclipse"     # runs in active pane
+run_cmd "./eclimd"
+#run_cmd "date" 1  # runs in pane 1
+
+# Paste text
+send_keys "./eclimd"    # paste into active pane
+#send_keys "date" 1 # paste into pane 1
+
+# Set active pane.
+#select_pane 0
