dc3913942ab4906b14936ff1c95ec390588aa1b0
phcerdan <pablo.hernandez.cerdan@outlook.com>
2015-08-27 16:41:19 +1200

    Add link to .eclimrc in setup

--- setupScript.sh
+++ setupScript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo "Trying to setup following simlinks: .vimrc ; .tmux.conf ; .vimperatorrc ; .tmuxifier/layouts; .vim/UltiSnips"
+echo "Trying to setup following simlinks: .vimrc ; .tmux.conf ; .vimperatorrc ; .tmuxifier/layouts; .vim/UltiSnips ; .eclimrc"
 cd $HOME
 ln -s $SCRIPT_DIR/dot_files/.vimrc .
 ln -s $SCRIPT_DIR/dot_files/.tmux.conf .
 ln -s $SCRIPT_DIR/dot_files/.vimperatorrc .
+ln -s $SCRIPT_DIR/eclipse/.eclimrc .
 cd $HOME/.tmuxifier
 ln -s $SCRIPT_DIR/tmuxifier/layouts .
 cd $HOME/.vim
