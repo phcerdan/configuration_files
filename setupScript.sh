725e5d6cb8cf3ab9d57f0c2e9d07df612686b653
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-10-30 14:55:37 -0400

    GNOME: Change border to be black for all gtk windows.

--- setupScript.sh
+++ setupScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo "Trying to setup following simlinks: .vimrc ; .tmux.conf .tmux_utils ; .zshrc, .aliases, .functions, .devenviron; .vimperatorrc ; .tmuxifier/layouts; .vim/UltiSnips ; .eclimrc ; ycm_extra_conf.py ; colorgcc ; .spacemacs ; .gitconfig ; .git_template ; .fzf.conf ; .config/cower/config ; .config/termite/config ; .config/nyaovim/nyaovimrc.html ; .config/pycodestyle "
+echo "Trying to setup following simlinks: .vimrc ; .tmux.conf .tmux_utils ; .zshrc, .aliases, .functions, .devenviron; .vimperatorrc ; .tmuxifier/layouts; .vim/UltiSnips ; .eclimrc ; ycm_extra_conf.py ; colorgcc ; .spacemacs ; .gitconfig ; .git_template ; .fzf.conf ; .config/cower/config ; .config/termite/config ; .config/gtk-3.0/gtk.css ;.config/nyaovim/nyaovimrc.html ; .config/pycodestyle "
 cd $HOME
 ln -s $SCRIPT_DIR/dot_files/.vimrc .
 ln -s $SCRIPT_DIR/dot_files/.tmux.conf .
@@ -38,6 +38,8 @@ mkdir -p $HOME/.config/nyaovim ; cd $_
 ln -s $SCRIPT_DIR/dot_files/config/nyaovim/nyaovimrc.html .
 mkdir -p $HOME/.config/termite ; cd $_
 ln -s $SCRIPT_DIR/dot_files/config/termite/config .
+mkdir -p $HOME/.config/gtk-3.0 ; cd $_
+ln -s $SCRIPT_DIR/dot_files/config/gtk-3.0/gtk.css .
 cd $HOME/.config
 ln -s $SCRIPT_DIR/dot_files/config/pycodestyle .
 ln -s $SCRIPT_DIR/dot_files/config/flake8 .
