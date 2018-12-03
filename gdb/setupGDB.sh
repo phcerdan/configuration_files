f50cb90d0c0e7df394bb424ffdc74898c5f52c8f
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-12-03 11:18:10 -0500

    GDB: restore ~/.gdb in setupGDB

--- gdb/setupGDB.sh
+++ gdb/setupGDB.sh
@@ -15,13 +15,13 @@ else
     echo "$HOME/.gdbinit.d directory exists. Symlink not created."
 fi
 
-# # .gdb folder
-# if [ ! -d "$HOME/.gdb" ]; then
-#     SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-#     ln -s $SCRIPT_DIR/.gdb $HOME
-#     # install external git pretty-printers. External repos are ignored from .gitignore
-#     git clone https://github.com/mateidavid/Boost-Pretty-Printer ~/.gdb/Boost-Pretty-Printer
-# else
-#     echo "$HOME/.gdb folder exists. Do Nothing."
-# fi
+# .gdb folder
+if [ ! -d "$HOME/.gdb" ]; then
+    SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    ln -s $SCRIPT_DIR/.gdb $HOME
+    # install external git pretty-printers. External repos are ignored from .gitignore
+    git clone https://github.com/mateidavid/Boost-Pretty-Printer ~/.gdb/Boost-Pretty-Printer
+else
+    echo "$HOME/.gdb folder exists. Do Nothing."
+fi
 
