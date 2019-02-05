75da65d8808574a47aaf43cef15a4ab3ebd8ca37
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-02-05 15:18:17 -0500

    Add coc_settings (with ccls) to setupScript

--- setupScript.sh
+++ setupScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo "Trying to setup following simlinks: .vimrc ; .tmux.conf .tmux_utils ; .zshrc, .aliases, .functions, .devenviron; .vimperatorrc ; .tmuxifier/layouts; .vim/UltiSnips ; .eclimrc ; ycm_extra_conf.py ; colorgcc ; .spacemacs ; .gitconfig ; .git_template ; .fzf.conf ; .config/cower/config ; .config/termite/config ; .config/gtk-3.0/gtk.css ;.config/nyaovim/nyaovimrc.html ; .config/pycodestyle "
+echo "Trying to setup following simlinks: .vimrc ; .tmux.conf .tmux_utils ; .zshrc, .aliases, .functions, .devenviron; .vimperatorrc ; .tmuxifier/layouts; .vim/UltiSnips ; .eclimrc ; ycm_extra_conf.py ; colorgcc ; .spacemacs ; .gitconfig ; .git_template ; .fzf.conf ; .config/cower/config ; .config/termite/config ; .config/gtk-3.0/gtk.css ; .config/pycodestyle "
 cd $HOME
 ln -s $SCRIPT_DIR/dot_files/.vimrc .
 ln -s $SCRIPT_DIR/dot_files/.tmux.conf .
@@ -34,12 +34,12 @@ ln -s $SCRIPT_DIR/vim/UltiSnips .
 ln -s $SCRIPT_DIR/vim/ftplugin .
 mkdir -p $HOME/.config/cower ; cd $_
 ln -s $SCRIPT_DIR/dot_files/config/cower/config .
-mkdir -p $HOME/.config/nyaovim ; cd $_
-ln -s $SCRIPT_DIR/dot_files/config/nyaovim/nyaovimrc.html .
 mkdir -p $HOME/.config/termite ; cd $_
 ln -s $SCRIPT_DIR/dot_files/config/termite/config .
 mkdir -p $HOME/.config/gtk-3.0 ; cd $_
 ln -s $SCRIPT_DIR/dot_files/config/gtk-3.0/gtk.css .
+mkdir -p $HOME/.config/nvim ; cd $_
+ln -s $SCRIPT_DIR/dot_files/config/nvim/coc-settings.json .
 cd $HOME/.config
 ln -s $SCRIPT_DIR/dot_files/config/pycodestyle .
 ln -s $SCRIPT_DIR/dot_files/config/flake8 .
