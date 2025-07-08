e716004358a6e72d64c99e305e929fb895cee9bf
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2025-07-08 16:35:05 +0200

    Remove unused from setupScript

--- setupScript.sh
+++ setupScript.sh
@@ -13,36 +13,36 @@ ln -s $SCRIPT_DIR/dot_files/.fzf.conf .
 ln -s $SCRIPT_DIR/dot_files/.aliases .
 ln -s $SCRIPT_DIR/dot_files/.functions .
 ln -s $SCRIPT_DIR/dot_files/.devenviron .
-ln -s $SCRIPT_DIR/dot_files/.vimperatorrc .
-ln -s $SCRIPT_DIR/dot_files/.ycm_extra_conf.py .
+#ln -s $SCRIPT_DIR/dot_files/.vimperatorrc .
+#ln -s $SCRIPT_DIR/dot_files/.ycm_extra_conf.py .
 ln -s $SCRIPT_DIR/dot_files/.colorgcc .
 ln -s $SCRIPT_DIR/dot_files/.colorgcc .colorgccrc
 ln -s $SCRIPT_DIR/dot_files/.latexmkrc .
 ln -s $SCRIPT_DIR/dot_files/.Xdefaults .
 ln -s $SCRIPT_DIR/dot_files/.Xresources .
 ln -s $SCRIPT_DIR/dot_files/.Xresources.d .
-ln -s $SCRIPT_DIR/eclipse/.eclimrc .
-ln -s $SCRIPT_DIR/emacs/.spacemacs .
+#ln -s $SCRIPT_DIR/eclipse/.eclimrc .
+#ln -s $SCRIPT_DIR/emacs/.spacemacs .
 ln -s $SCRIPT_DIR/git_files/.gitconfig .
 ln -s $SCRIPT_DIR/git_files/.git_template .
 ln -s $SCRIPT_DIR/git_files/.gitignore .
-ln -s $SCRIPT_DIR/gdb/minimal_gdbinit ./.gdbinit
+#ln -s $SCRIPT_DIR/gdb/minimal_gdbinit ./.gdbinit
 mkdir -p $HOME/.tmuxifier ; cd $_
 ln -s $SCRIPT_DIR/tmuxifier/layouts .
 mkdir -p $HOME/.vim ; cd $_
-ln -s $SCRIPT_DIR/vim/UltiSnips .
-ln -s $SCRIPT_DIR/vim/ftplugin .
-mkdir -p $HOME/.config/cower ; cd $_
-ln -s $SCRIPT_DIR/dot_files/config/cower/config .
-mkdir -p $HOME/.config/termite ; cd $_
-ln -s $SCRIPT_DIR/dot_files/config/termite/config .
-mkdir -p $HOME/.config/gtk-3.0 ; cd $_
-ln -s $SCRIPT_DIR/dot_files/config/gtk-3.0/gtk.css .
+#ln -s $SCRIPT_DIR/vim/UltiSnips .
+#ln -s $SCRIPT_DIR/vim/ftplugin .
+#mkdir -p $HOME/.config/cower ; cd $_
+#ln -s $SCRIPT_DIR/dot_files/config/cower/config .
+#mkdir -p $HOME/.config/termite ; cd $_
+#ln -s $SCRIPT_DIR/dot_files/config/termite/config .
+#mkdir -p $HOME/.config/gtk-3.0 ; cd $_
+#ln -s $SCRIPT_DIR/dot_files/config/gtk-3.0/gtk.css .
 mkdir -p $HOME/.config/nvim ; cd $_
-ln -s $SCRIPT_DIR/dot_files/config/nvim/coc-settings.json .
-cd $HOME/.config
-ln -s $SCRIPT_DIR/dot_files/config/pycodestyle .
-ln -s $SCRIPT_DIR/dot_files/config/flake8 .
+#ln -s $SCRIPT_DIR/dot_files/config/nvim/coc-settings.json .
+#cd $HOME/.config
+#ln -s $SCRIPT_DIR/dot_files/config/pycodestyle .
+#ln -s $SCRIPT_DIR/dot_files/config/flake8 .
 
 echo "Cloning (git) Plugin managers in .vim (Vundle) and .tmux (tpm)"
 # zsh manager zgen, should be handled in .zshrc
