b424957ec4afcd5283fb26060bf7d89bf46241c7
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2025-11-10 17:39:46 +0100

    nvim: Remove deprecated warnings

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,26 +44,6 @@ local formatters = {
   cmake = { "cmakelang" },
 }
 
-local function create_cmd_FormatEnable()
-  vim.api.nvim_create_user_command("FormatEnable", function(args)
-    if args.bang then
-      -- FormatEnable! will enable formatting on save just for this buffer
-      vim.b.enable_autoformat = true
-    else
-      vim.g.enable_autoformat = true
-    end
-  end, {
-    desc = "Enable autoformat-on-save",
-    bang = true,
-  })
-  vim.api.nvim_create_user_command("FormatDisable", function()
-    vim.g.enable_autoformat = false
-    vim.b.enable_autoformat = false
-  end, {
-    desc = "Disable autoformat-on-save",
-  })
-end
-
 local debuggers = {
   python = { "debugpy" },
   cpp = {"cpptools"},
@@ -81,28 +61,22 @@ local dontInstall = {
 
 ---given the linter- and formatter-list of nvim-lint and conform.nvim, extract a
 ---list of all tools that need to be auto-installed
----@param myLinters object[]
----@param myFormatters object[]
----@param myDebuggers string[]
----@param ignoreTools string[]
----@return string[] tools
----@nodiscard
 local function toolsToAutoinstall(myLinters, myFormatters, myDebuggers, ignoreTools)
   -- get all linters, formatters, & debuggers and merge them into one list
-  local linterList = vim.tbl_flatten(vim.tbl_values(myLinters))
-  local formatterList = vim.tbl_flatten(vim.tbl_values(myFormatters))
+  local linterList = vim.iter(vim.tbl_values(myLinters)):flatten(math.huge):totable()
+  local formatterList = vim.iter(vim.tbl_values(myFormatters)):flatten(math.huge):totable()
   local tools = vim.list_extend(linterList, formatterList)
   vim.list_extend(tools, myDebuggers)
 
   -- only unique tools
   table.sort(tools)
-  tools = vim.fn.uniq(tools)
+  local utools = vim.fn.uniq(tools)
 
   -- remove exceptions not to install
-  tools = vim.tbl_filter(function(tool)
+  utools = vim.tbl_filter(function(tool)
     return not vim.tbl_contains(ignoreTools, tool)
   end, tools)
-  return tools
+  return utools
 end
 
 require("lazy").setup({
@@ -112,12 +86,28 @@ require("lazy").setup({
     lazy = false,
     ---@type snacks.Config
     opts = {
-      git = {
-        enabled = true,
-      },
-      gh = {
-      },
+      bigfile = { enabled = true },
+      bufdelete = { enabled = true },
+      dashboard = { enabled = true },
+      debug = { enabled = true },
+      explorer = { enabled = true },
+      gh = { enabled = true },
+      git = { enabled = true },
+      gitbrowse = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scratch = { enabled = true },
+      scroll = { enabled = false }, -- Don't like the feeling.
+      statuscolumn = { enabled = true },
+      terminal = { enabled = true },
+      toggle = { enabled = true },
+      win = { enabled = true },
+      words = { enabled = true },
       picker = {
+        enabled = true,
         sources = {
           gh_issue = {
           },
@@ -125,15 +115,173 @@ require("lazy").setup({
           }
         }
       },
-      image = {
-        enabled = false,
-      },
+      image = { enabled = false },
     },
     keys = {
+      -- Top Pickers & Explorer
+      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      -- find
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      -- { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+      -- git
+      { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+      { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+      { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+      { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+      { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+      { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+      -- gh
       { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
       { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
       { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
       { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+      -- Grep
+      { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+      { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
+      -- search
+      { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
+      { '<leader>s/', function() Snacks.picker.search_history() end, desc = "Search History" },
+      { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+      { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+      { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+      { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+      { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+      { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+      { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+      { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+      { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+      { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+      { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+      { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+      { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+      { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+      { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+      { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+      { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+      -- LSP
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+      { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { "gai", function() Snacks.picker.lsp_incoming_calls() end, desc = "C[a]lls Incoming" },
+      { "gao", function() Snacks.picker.lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
+      { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+      { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+      -- Other
+      { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+      { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+      { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+      { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+      { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+      { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+      { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+      {
+        "<leader>N",
+        desc = "Neovim News",
+        function()
+          Snacks.win({
+            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+            width = 0.6,
+            height = 0.6,
+            wo = {
+              spell = false,
+              wrap = false,
+              signcolumn = "yes",
+              statuscolumn = " ",
+              conceallevel = 3,
+            },
+          })
+        end,
+      }
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+
+
+          -- Override print to use snacks for `:=` command
+          if vim.fn.has("nvim-0.11") == 1 then
+            vim._print = function(_, ...)
+              dd(...)
+            end
+          else
+            vim.print = _G.dd
+          end
+
+          -- Create some toggle mappings
+          Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+          Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+          Snacks.toggle.diagnostics():map("<leader>ud")
+          Snacks.toggle.line_number():map("<leader>ul")
+          Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+          Snacks.toggle.treesitter():map("<leader>uT")
+          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
+          Snacks.toggle.indent():map("<leader>ug")
+          Snacks.toggle.dim():map("<leader>uD")
+        end,
+      })
+    end,
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
     },
   },
   {
@@ -174,7 +322,8 @@ require("lazy").setup({
         highlight = {
           enable = true,
           disable = function(lang, bufnr) -- Disable in files with many lines or a really large first line (json)
-            local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+            -- local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+            local ft = vim.api.nvim_get_option_value( "filetype", {buf = bufnr})
             if ft == "text" or ft == "qf" then
               return true
             end
@@ -402,7 +551,6 @@ require("lazy").setup({
   { "jpalardy/vim-slime",               lazy = false }, -- Send/Copy from vim to other tmux pane
   -- Utils
   { "jbyuki/one-small-step-for-vimkind" },           -- dap adapter for lua running inside neovim
-  { "folke/neodev.nvim",                config = true }, -- Additional lua configuration, makes nvim stuff amazing
   { "nvim-tree/nvim-web-devicons" },
   {
     "folke/trouble.nvim",
@@ -580,7 +728,7 @@ require("lazy").setup({
   },
   {
    "mason-org/mason-lspconfig.nvim",
-   version = "1.32.0" 
+   version = "1.32.0"
   },
   {
     "neovim/nvim-lspconfig",
@@ -591,9 +739,6 @@ require("lazy").setup({
 
       -- Useful status updates for LSP
       { "j-hui/fidget.nvim", branch = "legacy" },
-
-      -- Additional lua configuration, makes nvim stuff amazing
-      "folke/neodev.nvim",
     },
   },
   { -- auto-install missing linters & formatters
@@ -1159,6 +1304,7 @@ require("lazy").setup({
     config = function()
       require("neotest").setup({
         status = {
+          enabled = true,
           virtual_text = true,
         },
         output = {
@@ -1166,7 +1312,7 @@ require("lazy").setup({
           open_on_run = "short"
         },
         quickfix = {
-          enable = true,
+          enabled = true,
           open = function()
           -- if vim.fn.exists(":Trouble") == 2 then
             require("trouble").open({ mode = "quickfix", focus = false })
@@ -1202,36 +1348,6 @@ require("lazy").setup({
 
   -- Copilot --
   { "github/copilot.vim",         lazy = false },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    dependencies = {
-      { "github/copilot.vim" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    config = function()
-      local chat = require("CopilotChat")
-      local select = require("CopilotChat.select")
-      chat.setup({
-        debug = false, -- Enable debugging
-        -- See Configuration section for rest
-        window = {
-          layout = 'vertical',
-        },
-        mappings = {
-          reset = {
-            insert = '',
-            normal = '',
-          },
-        },
-      })
-      -- Restore CopilotChatBuffer
-      vim.api.nvim_create_user_command("CopilotChatBuffer", function(args)
-        chat.ask(args.args, { selection = select.buffer })
-      end, { nargs = "*", range = true })
-    end,
-    -- See Commands section for default commands if you want to lazy load on them
-  },
   {
     "olimorris/codecompanion.nvim",
     config = function()
@@ -1386,21 +1502,14 @@ vim.keymap.set("n", "<leader>sl", require("fzf-lua").grep_last, { desc = "[S]ear
 vim.keymap.set("n", "<leader>sw", require("fzf-lua").grep_cword, { desc = "[S]earch [W]ord" })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[d", function() vim.diagnostic.jump({count=-1, float=true}) end)
+vim.keymap.set("n", "]d", function() vim.diagnostic.jump({count=1, float=true}) end)
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(client, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local methods = vim.lsp.protocol.Methods
+local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
     if desc then
       desc = "LSP: " .. desc
@@ -1563,6 +1672,7 @@ local languages = {
   bash = { shfmt, shellcheck },
   zsh = { shfmt, shellcheck },
   -- lua = { stylua },
+  cmake = { cmake_lint },
 }
 
 local efmls_config = {
@@ -1605,15 +1715,6 @@ local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./lua/vim-snippets", "./lua/snippets" } })
-
-
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-end
 
 cmp.setup({
   snippet = {
